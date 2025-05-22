7e456ab88334244aade794a2a1d856894cb23d08
guelitoo <159180695+guelitoo@users.noreply.github.com>
2025-05-21 21:08:24 -0300

    Update UsuarioRepository.java

--- login/src/main/java/br/senai/login/repositories/UsuarioRepository.java
+++ login/src/main/java/br/senai/login/repositories/UsuarioRepository.java
@@ -1,15 +1,20 @@
-package br.senai.login.repositories;
+package br.senai.aluno.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import br.senai.login.entities.Usuario;
+import br.senai.aluno.entities.Aluno;
+import br.senai.aluno.entities.Jogo;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	
-	//Consulta para encontra o nome de usuario
-	Usuario findByNomeUsuario(String nomeUsuario);
-	
-	//Consulta para encontrar por email
-	Usuario findByEmail(String email);
+public interface UsuarioRepository extends JpaRepository<Aluno, Long> {
+    
+    // Consulta para encontrar por RM
+    Aluno findByRm(String rm);
+    
+    // Consulta para encontrar por email
+    Aluno findByEmail(String email);
+    
+    // Consulta para encontrar alunos por jogo favorito
+    List<Aluno> findByJogoFavorito(Jogo jogo);
 }
